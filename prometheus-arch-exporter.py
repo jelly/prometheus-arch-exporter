22810437d85316146edd764018b4fa8bc97c916c
Jelle van der Waa <jelle@vdwaa.nl>
2019-01-14 20:17:47 +0100

    Clean up imports, unused functions and whitespace

--- prometheus-arch-exporter.py
+++ prometheus-arch-exporter.py
@@ -1,18 +1,12 @@
 #!/usr/bin/python
 
-import os
-import sys
 import time
-import json
 from subprocess import check_output
 
-import prometheus_client
 from prometheus_client import start_http_server, Metric, REGISTRY
 
 
 class ArchCollector(object):
-    def __init__(self):
-        pass
 
     def collect(self):
         packages = len(check_output('checkupdates').split(b'->'))
@@ -23,7 +17,7 @@ class ArchCollector(object):
 
         arch_audit_text = check_output(['arch-audit', '-u'])
         security_issues = len([line for line in arch_audit_text.splitlines()[:-1] if 'from testing repos' not in line])
-        
+
         metric = Metric('arch_audit', 'Arch Audit Packages', 'gauge')
         metric.add_sample('arch_audit', value=(security_issues), labels={})
         yield metric
